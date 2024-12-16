724e5e15e99236c01de5a5c778f6fb22a4b0d781
gabriel-rosa-da-silva-nunes <gabrielrosadasilvanunes@gmail.com>
2024-12-16 14:07:57 -0300

    feat: add output apigateway endpoint

--- infra/api_gateway.tf
+++ infra/api_gateway.tf
@@ -2,3 +2,7 @@ resource "aws_apigatewayv2_api" "fastfood-api-gateway" {
     name = "Api gateway fastfood"
     protocol_type = "HTTP"
 }
+
+output "apigateway_endpoint" {
+  value = aws_apigatewayv2_api.fastfood-api-gateway.api_endpoint
+}
